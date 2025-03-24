74dae59353a146d32e51773cafb4701a467f8750
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2025-03-24 21:11:29 +0600

    want to create pull request

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
 app.use(express.json());
 
 
