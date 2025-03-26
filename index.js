e6cdca7a8782f2b02653d0ac95b6b6e40b201f84
bayzedahmedandthe <bayzedm784@.com>
2025-03-26 14:44:32 +0600

    create questions details api

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const systemTheme = process.env.SYSTEM_INFO
 const genAI = new GoogleGenerativeAI(process.env.AI_SECRET_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" , systemInstruction: systemTheme,});
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash", systemInstruction: systemTheme, });
 // Middlewares
 app.use(cors());
 app.use(express.json());
@@ -28,31 +28,31 @@ async function run() {
     try {
         const questionCollection = client.db("devDB").collection("questions");
 
-// Ai Assistance api 
-const chat = model.startChat({history:[]})
+        // Ai Assistance api 
+        const chat = model.startChat({ history: [] })
 
-app.post("/chat", async (req, res) => {
-    try {
-      const { message } = req.body;
-  
-      if (!message) {
-        return res.status(400).json({ error: "Message is required" });
-      }
-  
-      let result = await chat.sendMessageStream(message);
-      let responseText = "";
-  
-      for await (const chunk of result.stream) {
-        responseText += chunk.text();
-      }
-  
-      res.json({ response: responseText });
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
-  
+        app.post("/chat", async (req, res) => {
+            try {
+                const { message } = req.body;
+
+                if (!message) {
+                    return res.status(400).json({ error: "Message is required" });
+                }
+
+                let result = await chat.sendMessageStream(message);
+                let responseText = "";
+
+                for await (const chunk of result.stream) {
+                    responseText += chunk.text();
+                }
+
+                res.json({ response: responseText });
+            } catch (error) {
+                console.error("Error:", error);
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        });
+
 
 
 
@@ -60,12 +60,18 @@ app.post("/chat", async (req, res) => {
 
 
         // Questions related apis
-        app.post("/questions", async(req, res) => {
+        app.post("/questions", async (req, res) => {
             const question = req.body;
             const result = await questionCollection.insertOne(question);
             res.send(result);
         });
 
+        app.get("/questions/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await questionCollection.findOne(query);
+            res.send(result);
+        });
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
